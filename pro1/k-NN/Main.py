47036ecf7cc2c56d5b7549c214a8bfdd392170dc
s27247 <s27247@pjwstk.edu.pl>
2024-04-08 20:02:56 +0200

    added a graph feature

--- pro1/k-NN/Main.py
+++ pro1/k-NN/Main.py
@@ -1,6 +1,9 @@
 import csv
 import numpy as np
 import os
+import matplotlib.pyplot as plt
+
+from KNNClassifier import KNNClassifier
 
 
 def main(k, trSet, tstSet):
@@ -9,7 +12,6 @@ def main(k, trSet, tstSet):
     v_test = readVec(tstSet)
     cl_test = readClass(tstSet)
 
-    from KNNClassifier import KNNClassifier
     kNN = KNNClassifier(k, v_train, cl_train)
 
     results = kNN.evaluate(v_test)
@@ -19,6 +21,9 @@ def main(k, trSet, tstSet):
     accuracy = correct_predictions / total_samples
 
     print("Classified with accuracy:", accuracy * 100, "%")
+
+    plot_graph(trSet, tstSet)
+
 
     while True:
         usrVector = input("Enter a custom vector to classify as comma - separated values"
@@ -30,6 +35,31 @@ def main(k, trSet, tstSet):
         print("Evaluated class:", result[0])
 
 
+def plot_graph(training_set, test_set):
+    accuracies = []
+    k_values = []
+
+    v_train = readVec(training_set)
+    cl_train = readClass(training_set)
+    v_test = readVec(test_set)
+    cl_test = readClass(test_set)
+
+    for k in range(1, 101):
+        kNN = KNNClassifier(k, v_train, cl_train)
+        results = kNN.evaluate(v_test)
+        correct_predictions = sum(1 for pred, true in zip(results, cl_test) if pred == true)
+        total_samples = len(cl_test)
+        accuracy = correct_predictions / total_samples
+        accuracies.append(accuracy)
+        k_values.append(k)
+
+    plt.plot(k_values, accuracies, marker='o')
+    plt.title('Accuracy vs. k')
+    plt.xlabel('k')
+    plt.ylabel('Accuracy')
+    plt.xticks(np.arange(1, 101, step=5))
+    plt.grid(True)
+    plt.show()
 def readVec(fileName):
     read = []
     path = os.path.join("data", fileName)
